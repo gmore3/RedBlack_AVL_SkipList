bbf6ac1364da10939f30f7bc10d39a5a9b81b839
gmore3 <42781368+gmore3@users.noreply.github.com>
2019-05-10 08:31:20 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,76 +3,137 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-    AVLDictionary<String, String> dict = new AVLDictionary<String, String>();
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("**********************AVL TREE***********************");
+		SkipList sl = new SkipList();
+		
+		System.out.println("**********************SkipList***********************");
 		char c = 0;
 		do {
 			
 			//System.out.println("1. Insert into Tree.");
-            System.out.println("\nAVL Tree Operations\n");
+            System.out.println("\nSkip List Operations\n");
             
             System.out.println("1. Insert ");
-            System.out.println("2. Search");
-            System.out.println("3. Delete");
-            System.out.println("4. Inorder Representation");
-            System.out.println("5. Printing the tree");
+            System.out.println("2. Get the value");
+            System.out.println("3. Search an entry");
+            System.out.println("4. Get one row of the key passed as parameter");
+            System.out.println("5. Get the column of the key passed as parameter in a horizontal manner");
+            System.out.println("6. Empty Skiplist check");
+            System.out.println("7. Print the Skip List");
+            System.out.println("8. Size of the Skiplist");
             
             System.out.println();
-            System.out.println("Enter the operation to be carried out :");
+            System.out.println("Enter the operation to be carried out on the Skip List:");
 			int choice = sc.nextInt();
 			
 			switch(choice) {
 				case 1: 
-					System.out.println("Enter the  key value pairs to insert: ");
-					int i = 0; 
-					while(i < 4)
-					{
-						String str = sc.next();
-						String[] arr = str.split(",");
-						dict.root=dict.insert(dict.root, arr[0], arr[1]);
-						i++;
-					}
-					System.out.println("\n\n\nTree to to String\n\n\n"+dict.toString());
+					System.out.println("Enter the pair to insert: ");
+//					int i = 0;
+//					while(i < 4)
+//					{
+//					  sl.insert(sc.next(),sc.next());
+//					  i++;
+//					}
+					sl.insert("One", 1);
+					sl.insert("Four", 4);
+					sl.insert("Ten", 10);
+					sl.insert("Three", 3);
+					sl.insert("Eleven", 11);
+					sl.printHorizontal();
 					break;
-//				
-//				case 2: 
-//					System.out.println("Enter the element to be searched :");
-//					break;
-//			
-//				case 3:
-//					System.out.println("Enter the key value pair to be deleted :");
-//		     		
+					
+		     	case 2:
+					System.out.println("Enter the key whose associted value is required :");
+					Integer value = sl.get(sc.next()); // get the value of the key passed
+					System.out.print(value);
+					break;
+					
+				case 3:
+					System.out.println("Enter the key of the pair to be searched : ");
+					SkipListEntry value1 = sl.findEntry(sc.next()); // to find the entry of the key passed
+					System.out.print(value1);
+					break;
+						
+		     	case 4:
+					System.out.println("Get one row of the key passed as parameter : ");
+//					String value3 = sl.getOneRow(sc.next());
+//					System.out.println(value3);
+					break;
+					
+		     	case 5:
+		     		System.out.println("Get the column of the key passed as parameter in a horizontal manner : ");
+//		     		String value4 = sl.getOneColumn(sc.next());
+//		    		System.out.println(value4);
+					break;
+					
+		     	case 6:
+		     		System.out.println("Empty Skiplist check ");
+		    		boolean empty = sl.isEmpty();
+		    		System.out.println(empty);
+					break;
+		     		
+		     	case 7:
+		     		System.out.println("Print the Skip List : ");
+		     		sl.printHorizontal();
+					break;
+				
+		     	case 8:
+		     		System.out.println("Size of the Skip List : ");
+		    		int size = sl.size();
+		    		System.out.println(size);
+					break;
+					
 				default:
-					System.out.println("Invalid choice"); 
+					System.out.println("Invalid choice");
 					break;
 			}
 			
 			System.out.println("\nDo you want to continue (y/n): \n");
 			c = sc.next().charAt(0);
 		}while (c == 'y');
+		
             
-//		dict.printTree();
+         
+//		sl.insert("One", 1);
+//		System.out.println("\nPrinting the Skip List:\n");
+//		sl.insert("Four", 4);
+//		sl.insert("Ten", 10);
+//		sl.insert("Three", 3);
+//		sl.insert("Eleven", 11);
+//		sl.printHorizontal();
 //
-//		dict.root = dict.insert(dict.root, "13", "13");
-//		dict.root = dict.insert(dict.root, "10", "10");
-//		dict.root = dict.insert(dict.root, "15", "15");
-//		dict.root = dict.insert(dict.root, "05", "05");
-//		dict.root = dict.insert(dict.root, "11", "11");
-//		dict.root = dict.insert(dict.root, "16", "16");
-//		dict.root = dict.insert(dict.root, "04", "04");
-//		dict.root = dict.insert(dict.root, "06", "06");
+//		Integer value = sl.get("Eleven"); // get the value
+//		System.out.println();
+//		System.out.print(value);
+//		System.out.println();
 //
-//  	    dict.printTree();
-//  	    System.out.println("\n\n\nTree to to String\n\n\n"+dict.toString());
-//  	    System.out.println(dict.search("04"));
-//  	    
-//  	    StringBuilder s = new StringBuilder();
-//  	    s.append("hello ");
-//  	    s.append(true);
-//  	    System.out.println(s);
+//		System.out.println();
+//		SkipListEntry value1 = sl.findEntry("Eleven"); // to find the entry
+//		System.out.print(value1);
+//		System.out.println();
+//
+//		System.out.println(); // insert the key-value pair
+//		sl.insert("Sixty", 60);
+//		sl.printHorizontal();
+//
+//		System.out.println(); // get one row of the key passed as parameter
+//		String value3 = sl.getOneRow(value1);
+//		System.out.println(value3);
+//
+//		System.out.println(); // get the column of the key passed as parameter in a horizontal manner
+//		String value4 = sl.getOneColumn(value1);
+//		System.out.println(value4);
+//
+//		System.out.println(); // to check whether the skiplist is empty
+//		boolean bol1 = sl.isEmpty();
+//		System.out.println(bol1);
+//
+//		System.out.println(); // gives the size of the skiplist
+//		int bol2 = sl.size();
+//		System.out.println(bol2);
 
-		}
 	}
+}
